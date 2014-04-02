f6b3ccfbeac58fa811731b089e4a1099f52ab05a
bilalba <tringingly@gmail.com>
2014-04-02 21:25:08 +0500

    reversing the in.readLine

--- Serverthread.java
+++ Serverthread.java
@@ -17,19 +17,20 @@ public class Serverthread implements Runnable {
 	public void run() {
 		try {
 			System.out.println("A connection has been made.");
-		String h = "";
+		String h;
       	BufferedReader in = new BufferedReader(new InputStreamReader(df.getInputStream()));             
       	DataOutputStream out = new DataOutputStream(df.getOutputStream());
       	while (true) {
       		try{
-	      		h = in.readLine();
-	      	} catch (Exception e) {
-	      		System.out.println("Request timed out. 10 second time out.");
-	      	}
-	      	if (h.equals("")) {
-	      		System.out.println(h); 
-	      		out.writeBytes("WTF\n");
-	      	}
+  		      	
+		      	if ((h = in.readLine()) != null) {
+		      		System.out.println(h); 
+		      		out.writeBytes("WTF\n");
+		      	}
+			} catch (Exception e) {
+				System.out.println("Request Timed out. 10 sec.");
+			}
+
       }
 
 		} catch (Exception e) {
